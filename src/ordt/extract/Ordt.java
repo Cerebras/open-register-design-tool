5db1d156868bb7d8d3a39667ae224d87db390fdf
Jeff Ebert <jeffrey@ebertland.net>
2018-04-08 14:08:15 -0700

    Update version to follow internal convention

--- src/ordt/extract/Ordt.java
+++ src/ordt/extract/Ordt.java
@@ -30,7 +30,7 @@ import ordt.parameters.ExtParameters.UVMModelModes;
 
 public class Ordt {
 
-	private static String version = "180406.01-crb01"; 
+	private static String version = "180408.01-crb";
 	private static DebugController debug = new MyDebugController(); // override design annotations, input/output files
 
 	public enum InputType { RDL, JSPEC };
